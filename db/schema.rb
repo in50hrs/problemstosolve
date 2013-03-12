680d44b97b71c5c76c32474f083aac2f1408114e
Shayon Mukherjee <its@shayon.me>
2013-03-12 17:16:02 +0530

    Twitter setup done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130312091502) do
+ActiveRecord::Schema.define(:version => 20130312110909) do
 
   create_table "problems", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(:version => 20130312091502) do
     t.datetime "updated_at",                                            :null => false
     t.string   "username",               :limit => nil
     t.string   "name",                   :limit => nil
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
