a22918f2bb721cd9218f77c82cce7482a8e6cbd7
Shayon Mukherjee <its@shayon.me>
2013-03-13 23:45:19 +0530

    Working on setting up the problem. Tons of stuff remaining.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #raise request.env["omniauth.auth"].to_yaml
     if user.persisted?
           flash.notice = "Signed in!"
-          sign_in_and_redirect user
+          sign_in_and_redirect(user)
         else
           session["devise.user_attributes"] = user.attributes
           redirect_to new_user_registration_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,41 @@
 Problemstosolve::Application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  resources :users  
-  resources :problems 
+  resources :users  do
+  resources :problems
+end
+  match "/problems" => "problems#main"
   get "home/index"
   root :to => 'home#index'
 end
 
 
-# rake routes - updated - 2:49PM - 12/3/2012
+# rake routes - updated - 10:47PM - 13/3/2012
 
-# new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
-#            user_password POST   /users/password(.:format)      devise/passwords#create
-#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
-#                          PUT    /users/password(.:format)      devise/passwords#update
-# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
-#        user_registration POST   /users(.:format)               devise/registrations#create
-#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
-#                          PUT    /users(.:format)               devise/registrations#update
-#                          DELETE /users(.:format)               devise/registrations#destroy
-#                    users GET    /users(.:format)               users#index
-#                          POST   /users(.:format)               users#create
-#                 new_user GET    /users/new(.:format)           users#new
-#                edit_user GET    /users/:id/edit(.:format)      users#edit
-#                     user GET    /users/:id(.:format)           users#show
-#                          PUT    /users/:id(.:format)           users#update
-#                          DELETE /users/:id(.:format)           users#destroy
-#                 problems GET    /problems(.:format)            problems#index
-#                          POST   /problems(.:format)            problems#create
-#              new_problem GET    /problems/new(.:format)        problems#new
-#             edit_problem GET    /problems/:id/edit(.:format)   problems#edit
-#                  problem GET    /problems/:id(.:format)        problems#show
-#                          PUT    /problems/:id(.:format)        problems#update
-#                          DELETE /problems/:id(.:format)        problems#destroy
-#               home_index GET    /home/index(.:format)          home#index
-#                     root        /                              home#index
+# #new_user_session GET    /users/sign_in(.:format)                    devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)                    devise/sessions#create
+#     destroy_user_session GET    /users/sign_out(.:format)                   devise/sessions#destroy
+#  user_omniauth_authorize        /users/auth/:provider(.:format)             omniauth_callbacks#passthru {:provider=>/twitter|facebook|google_oauth2/}
+#   user_omniauth_callback        /users/auth/:action/callback(.:format)      omniauth_callbacks#(?-mix:twitter|facebook|google_oauth2)
+#            user_password POST   /users/password(.:format)                   devise/passwords#create
+#        new_user_password GET    /users/password/new(.:format)               devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)              devise/passwords#edit
+#                          PUT    /users/password(.:format)                   devise/passwords#update
+# cancel_user_registration GET    /users/cancel(.:format)                     devise/registrations#cancel
+#        user_registration POST   /users(.:format)                            devise/registrations#create
+#    new_user_registration GET    /users/sign_up(.:format)                    devise/registrations#new
+#   edit_user_registration GET    /users/edit(.:format)                       devise/registrations#edit
+#                          PUT    /users(.:format)                            devise/registrations#update
+#                          DELETE /users(.:format)                            devise/registrations#destroy
+#         new_user_problem GET    /users/:user_id/problems/new(.:format)      problems#new
+#        edit_user_problem GET    /users/:user_id/problems/:id/edit(.:format) problems#edit
+#             user_problem GET    /users/:user_id/problems/:id(.:format)      problems#show
+#                    users GET    /users(.:format)                            users#index
+#                          POST   /users(.:format)                            users#create
+#                 new_user GET    /users/new(.:format)                        users#new
+#                edit_user GET    /users/:id/edit(.:format)                   users#edit
+#                     user GET    /users/:id(.:format)                        users#show
+#                          PUT    /users/:id(.:format)                        users#update
+#                          DELETE /users/:id(.:format)                        users#destroy
+#                 problems        /problems(.:format)                         problems#index
+#               home_index GET    /home/index(.:format)                       home#index
+#                     root        /                                           home#index
